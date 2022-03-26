678c0b6a72f4f5900a3181e749366ec14eb77b9f
filawt <80281561+filawt@users.noreply.github.com>
2022-03-26 14:07:04 -0400

    Pulling more #AfricansinUkraine tweets

--- JSONParse.py
+++ JSONParse.py
@@ -1,24 +1,20 @@
 import requests
 import json
-headers = {'Bearer': 'AAAAAAAAAAAAAAAAAAAAACouawEAAAAAHRlgxSjjVR9Kgf3VmMp89FoHSao%3D8kmNQUznpGbHWsrqkitxgAaYjzSBI2qZvUo3va2jifj0ZwwWwI'}
-# req= requests.get("https://api.twitter.com/2/tweets/search/recent?query=cat%20has%3Amedia%20-grumpy&tweet.fields=created_at&max_results=100", headers)
-# response= req.json()
-# dataResponse= json.load(response) (read json file and save it to dataResponse)
-f= open('AfricansinUkraine.json')
-dataResponse= json.load(f)
-print(dataResponse)
-a = "AfricansinUkraine"
+headers = {'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAACouawEAAAAAHRlgxSjjVR9Kgf3VmMp89FoHSao%3D8kmNQUznpGbHWsrqkitxgAaYjzSBI2qZvUo3va2jifj0ZwwWwI'}
+req= requests.get("https://api.twitter.com/2/tweets/search/recent?query=%23AfricansinUkraine&max_results=100&until_id=1507670246093590529", headers=headers)
+response= req.json()
+#json.dump(response, 'AfricansinUkraineMax.json')
+#dataResponse= json.load(response)
+#f= open('AfricansinUkraineMax.json')
+#print(response)
+a = "AfricansinUkraineSecondBatch"
 i = 1
 
-for item in dataResponse["data"]:
+for item in response["data"]:
     b = a + str(i) + '.json'
     
     with open(b, 'w') as g:
 
         json.dump(item, g)
-    #print(item['data']['id'])
-    #print(item)
     i += 1
-f.close()
-# print(response)
     
