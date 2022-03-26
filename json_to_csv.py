e557a4788df2072353a1915542a9a5a39dba3fbd
James Liu <liuxjames88@gmail.com>
2022-03-26 16:46:35 -0400

    code for converting a json file to csv

--- json_to_csv.py
+++ json_to_csv.py
@@ -5,15 +5,24 @@ headers = {'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAACouawEAAAAAHRlgxSjjVR9K
 req= requests.get("https://api.twitter.com/2/tweets/search/recent?query=%23AfricansinUkraine&max_results=100&until_id=1507670246093590529", headers=headers)
 response= req.json()
 
-# file = open("test.json", "w")
+print(response)
+
+
+file = open("data/AfricansinPoland.json", "r")
+content = file.read()
+json_content = json.loads(content)
+df = pd.DataFrame.from_dict(json_content['data'])
+print(df)
+df.to_csv(r'test2.csv', index=False, header=True)
+
 # file.write(response)
 # file.close()
 # print(type(response))
 # print(response)
 
-df = pd.DataFrame.from_dict(response['data']) 
-print(df)
-df.to_csv (r'test.csv', index = False, header=True)
+# df = pd.DataFrame.from_dict(response['data']) 
+# print(df)
+# df.to_csv (r'test.csv', index = False, header=True)
 
 # print(response)
 # pandas.json_to_csv(response)
