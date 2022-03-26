bfc5a9a37a01fc05e5420dee9324f7832699772e
filawt <80281561+filawt@users.noreply.github.com>
2022-03-26 15:25:08 -0400

    #AfricansinPoland tweets

--- AfricansinUkraine.py
+++ AfricansinUkraine.py
@@ -3,11 +3,14 @@ import json
 headers = {'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAACouawEAAAAAHRlgxSjjVR9Kgf3VmMp89FoHSao%3D8kmNQUznpGbHWsrqkitxgAaYjzSBI2qZvUo3va2jifj0ZwwWwI'}
 req= requests.get("https://api.twitter.com/2/tweets/search/recent?query=%23AfricansinUkraine&max_results=100&until_id=1507670246093590529", headers=headers)
 response= req.json()
-#json.dump(response, 'AfricansinUkraineMax.json')
-#dataResponse= json.load(response)
-#f= open('AfricansinUkraineMax.json')
-#print(response)
-a = "AfricansinUkraineSecondBatch"
+# saving all tweets to AfricansinUkraine.json files. w= write, += creates if file doesn't exist, or replaces file if file exists
+f= open('AfricansinUkraine.json', "w+")
+json.dump(response, f)
+print(f.read())
+# print(response)
+
+# Saving objects in AfricansinUkraine into their own json files
+a = "AfricansinUkraine"
 i = 1
 
 for item in response["data"]:
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # refugee-data
+
+In this project we are scraping tweets from API with hashtags such as #AfricansinUkraine and #AfricansinPoland to analyze the negative sentiment against non-white refugees fleeing the war in Ukraine.
