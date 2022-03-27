d3f1a6ab32bb99804ce02752484c4c22c62f0b88
eryn <sophia.buhat@google.com>
2022-03-27 16:05:19 -0400

    resources component
    can use this to list sources, links, etc

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,21 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { DescComponent } from './desc/desc.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    DescComponent
+    DescComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'resources', component: ResourcesComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
