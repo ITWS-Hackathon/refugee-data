9813f798299398ed47e2dcfeaba33922751b112c
James Liu <liuxjames88@gmail.com>
2022-03-27 15:46:37 -0400

    adding endpoints for getting data from filesystem

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000
+const fs = require('fs')
 const path = require('path'); 
 
 app.use(express.static('public'));
 
 app.use(express.static(path.join(__dirname, '../frontend/dist/refugeesite'))); 
 
-app.listen(port, () => {
-  console.log('Listening on *:3000')
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`)
 });
+
+app.get('/continents', (req, res) => {
+    let rawdata = fs.readFileSync('data_continents.json');
+    let data = JSON.parse(rawdata);
+    // console.log(data)
+    res.status(200).send(data)
+})
+
+
+app.get('/countries', (req, res) => {
+  let rawdata = fs.readFileSync('data_countries.json');
+  let data = JSON.parse(rawdata);
+  // console.log(data)
+  res.status(200).send(data)
+})
