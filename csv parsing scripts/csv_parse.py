57e2c9e0b71423ca68ac9de9430a30ceb37db89c
James Liu <liuxjames88@gmail.com>
2022-03-27 15:10:17 -0400

    python graphing stuff

--- csv parsing scripts/csv_parse.py
+++ csv parsing scripts/csv_parse.py
@@ -11,38 +11,65 @@ def main():
 
     test = pd.read_csv(f'./{sys.argv[1]}')
 
-    test_trimmed = pd.DataFrame();
-
-    test_trimmed['location'] = test['Indicator']
-    test_trimmed['TIME'] = test['TIME']
-    test_trimmed['Time'] = test['Time']
-    test_trimmed['Value'] = test['Value']
+    df = pd.DataFrame();
+    locations, values, years = [], [], []
 
 
-    locations, values_2015, values_2016, values_2017, values_2018, values_2019, values_2020 = [], [], [], [], [], [], []
+    for i in range(0, len(test), 6):
+        if ('female' in test.iloc[i]['Indicator'] or 'male' in test.iloc[i]['Indicator']):
+            continue
+        temp1 = []
+        temp2 = []
+        for j in range(6):
+            values.append(test.iloc[i+j]['Value'])
+            years.append(str(2015 + j))
+            locations.append(test.iloc[i]['Indicator'])
 
-    for i in range(0, len(test_trimmed), 6):
-        locations.append(test_trimmed.iloc[i]['location'])
-        values_2015.append(test_trimmed.iloc[i]['Value'])
-        values_2016.append(test_trimmed.iloc[i]['Value'])
-        values_2017.append(test_trimmed.iloc[i]['Value'])
-        values_2018.append(test_trimmed.iloc[i]['Value'])
-        values_2019.append(test_trimmed.iloc[i]['Value'])
-        values_2020.append(test_trimmed.iloc[i]['Value'])
+        # print(test.iloc[i]['Indicator'], "test")
 
-    test2 = pd.DataFrame();
-    test2['Inbound Country'] = locations
-    test2['2015'] = values_2015
-    test2['2016'] = values_2016
-    test2['2017'] = values_2017
-    test2['2018'] = values_2018
-    test2['2019'] = values_2019
-    test2['2020'] = values_2020
+        # values.append(temp1)
+        # years.append(temp2)
 
-    # print(test2)
-    # print(test2.head())
+    df['Location'] = locations
+    df['Year'] = years
+    df['Value'] = values
 
-    test2.to_csv(f'{sys.argv[2]}', sep='\t')
+    # df['location'] = test['Indicator']
+    # df['Year'] = test['TIME']
+    # df['Value'] = test['Value']
+
+    df.to_csv(f'{sys.argv[2]}', sep=',', index=False)
+    df.to_json('data_continents.json')
+
+
+
+    # df.drop(df.columns[df.apply(lambda col: "female" in col or "male" in col)])
+
+    # df.to_csv(f'{sys.argv[2]}', sep=',', index=False)
+    # locations, values_2015, values_2016, values_2017, values_2018, values_2019, values_2020 = [], [], [], [], [], [], []
+
+    # for i in range(0, len(df), 6):
+    #     locations.append(df.iloc[i]['location'])
+    #     values_2015.append(df.iloc[i]['Value'])
+    #     values_2016.append(df.iloc[i+1]['Value'])
+    #     values_2017.append(df.iloc[i+2]['Value'])
+    #     values_2018.append(df.iloc[i+3]['Value'])
+    #     values_2019.append(df.iloc[i+4]['Value'])
+    #     values_2020.append(df.iloc[i+5]['Value'])
+
+    # test2 = pd.DataFrame();
+    # test2['Inbound Location'] = locations
+    # test2['2015'] = values_2015
+    # test2['2016'] = values_2016
+    # test2['2017'] = values_2017
+    # test2['2018'] = values_2018
+    # test2['2019'] = values_2019
+    # test2['2020'] = values_2020
+
+    # # print(test2)
+    # # print(test2.head())
+
+    # test2.to_csv(f'{sys.argv[2]}', sep=',', index=False)
 
 
 
