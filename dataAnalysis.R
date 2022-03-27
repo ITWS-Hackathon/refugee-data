db2c180c2347c85f46083d6c339b4bc872ca27e3
luy17 <80281609+luy17@users.noreply.github.com>
2022-03-27 18:53:33 -0400

    Update dataAnalysis.R

--- dataAnalysis.R
+++ dataAnalysis.R
@@ -9,17 +9,16 @@ tweetDS <- read.csv("/Users/yasmine/Downloads/github/jjsalesplatform/refugee-dat
 text <- tweetDS$Words
 docs <- Corpus(VectorSource(text))
 
-docs <- tm::tm_map(docs, content_transformer(tolower))
+#docs <- tm::tm_map(docs, content_transformer(tolower))
 #docs <- tm_map(docs, removeWords, stopwords("english"))
 
 dtm <- TermDocumentMatrix(docs) 
 matrix <- as.matrix(dtm) 
 words <- sort(rowSums(matrix),decreasing=TRUE) 
 df <- data.frame(word = names(words),freq=words)
-df = df[-1,]
 
-set.seed(1234) # for reproducibility 
-# wordcloud(words = df$word, freq = df$freq, min.freq = 2, max.words=200, random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
+#set.seed(1234) # for reproducibility 
+#wordcloud(words = df$word, freq = df$freq, min.freq = 2, max.words=200, random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
 wordcloud2(data=df, size=1.5, color='random-dark', rotateRatio = 1)
 # letterCloud(data=df, "Ukraine", color='random-dark', size=1, backgroundColor="white")
            
